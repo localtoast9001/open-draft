864f76458df65d8514be11fd0f9292885f44f8cf
localtoast9001 <motherplucker@gmail.com>
2026-06-14 13:29:15 -0700

    add initial .NET project structure with CI/CD configuration and StyleCop settings (#2)
    * add initial .NET project structure with CI/CD configuration and StyleCop settings
    
    * Fix output directory to avoid file conflicts during build.

---
+++ net/OpenDraft.Core.UnitTest/MSTestSettings.cs
@@ -0,0 +1,5 @@
+﻿// <copyright file="MSTestSettings.cs" company="Jon Rowlett">
+// Copyright (c) Jon Rowlett. All rights reserved.
+// </copyright>
+
+[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
